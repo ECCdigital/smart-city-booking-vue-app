68b2a88d884153883ea425ba7415b61d33ae4bcc
Marvin Anders <marvin.anders@e-c-crew.de>
2024-05-24 10:27:39 +0200

    Updated pareva informations

---
+++ src/components/CommonComponents/PasswordField.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+  name: "PasswordField",
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      default: "Passwort",
+    },
+    color: {
+      type: String,
+      default: "accent",
+    },
+  },
+  data() {
+    return {
+      password: this.value,
+      show: false,
+    };
+  },
+  methods: {
+    updateValue() {
+      this.$emit("input", this.password);
+    },
+  },
+}
+</script>
+
+<template>
+  <v-text-field
+    :background-color="color"
+    filled
+    dense
+    :label="label"
+    v-model="password"
+    @input="updateValue"
+    :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+    @click:append="show = !show"
+    :type="show ? 'text' : 'password'"
+  ></v-text-field>
+</template>
+
+<style scoped>
+
+</style>
