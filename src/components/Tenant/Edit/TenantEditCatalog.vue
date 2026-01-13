22c5c4f293d1e9608bdccc3d4bb6efed6f23d31c
Marvin Anders <marvin.anders@e-c-crew.de>
2026-01-13 10:37:56 +0100

    fixed watcher

--- src/components/Tenant/Edit/TenantEditCatalog.vue
+++ src/components/Tenant/Edit/TenantEditCatalog.vue
@@ -15,12 +15,13 @@ export default {
   data() {
     return {
       valid: false,
-      localTenant: { },
+      localTenant: { catalogParticipation: DEFAULT_CATALOG_PARTICIPATION },
     };
   },
   watch: {
     tenant: {
       deep: true,
+      immediate: true,
       handler(v) {
         this.localTenant = { ...v };
         this.ensureCatalogParticipation();
@@ -51,11 +52,22 @@ export default {
 </script>
 
 <template>
-  <BaseSection title="Katalog Konfiguration" icon="mdi-book-open-page-variant" hint="Dieser Service kann im instanzweiten Katalog angezeigt werden. Der Katalog ist öffentlich zugänglich         — zusätzlich können Sie die Anzeige auf registrierte Nutzer einschränken.">
-
-    <v-alert type="info" dense border="left" elevation="1" colored-border max-width="750px">
-      Der Katalog ist instanzweit. Sichtbarkeitseinstellungen hier beeinflussen lediglich,
-      ob dieses Angebot im Katalog erscheint und ob es für alle oder nur für registrierte Nutzer sichtbar ist.
+  <BaseSection
+    title="Katalog Konfiguration"
+    icon="mdi-book-open-page-variant"
+    hint="Dieser Service kann im instanzweiten Katalog angezeigt werden. Der Katalog ist öffentlich zugänglich         — zusätzlich können Sie die Anzeige auf registrierte Nutzer einschränken."
+  >
+    <v-alert
+      type="info"
+      dense
+      border="left"
+      elevation="1"
+      colored-border
+      max-width="750px"
+    >
+      Der Katalog ist instanzweit. Sichtbarkeitseinstellungen hier beeinflussen
+      lediglich, ob dieses Angebot im Katalog erscheint und ob es für alle oder
+      nur für registrierte Nutzer sichtbar ist.
     </v-alert>
 
     <v-switch
@@ -71,6 +83,4 @@ export default {
   </BaseSection>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
